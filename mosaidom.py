911b5c2837d7ea820c2250713dfcf58c28db7a2d
Kyohei SAKAI <46491261+kyhs@users.noreply.github.com>
2022-03-24 03:01:08 +0900

    Update mosaidom.py
    update the loop

--- mosaidom.py
+++ mosaidom.py
@@ -12,20 +12,38 @@ def calc_square():
     while True:
         min_x = 1000
         min_y = 1000
+        counter = 0
+        i = 0
         for ul_point in ul_list:
             if min_y > ul_point[1]:
                 min_y = ul_point[1] # min_y is minimum y-axis value in ul_point
                 if min_x > ul_point[0]:
                     min_x = ul_point[0] # min_x is minimum x-axis value in min_y line
+                    i = counter
+            counter += 1
         
         # upper left point in this loop
         p_x = min_x
         p_y = min_y
+        del ul_list[i] # remove point p from ul_list
         print(p_x, p_y)
 
-        max_l = 100000 # TODO consider the case of stick out
-        length = random.randint(5, 10)  # TODO when margin is less than 5
-        break
+        max_l = min(width - p_x, height - p_y) # TODO consider the case of stick out
+        length = random.randint(5, max_l)  # TODO when margin is less than 5
+        ans_list.append([p_x, p_y, length])
+
+        # append new ul to ul_list
+        if p_x + length < width: # TODO consider the case of stick out
+            ul_list.append([p_x + length + 1, p_y])
+        if p_y + length < height: # TODO consider the case of stick out
+            ul_list.append([p_x, p_y + length + 1])
+
+        print(ul_list.len())
+        print('\n')
+
+        if len(ul_list) == 0:
+            break
+        # break
     return
 
 if __name__ == "__main__":
